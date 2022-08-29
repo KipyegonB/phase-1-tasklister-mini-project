6dc01097376b0484a50461a2fde23d69b1af22f1
KipyegonB <koechbernard08@gmail.com>
2022-08-29 13:53:26 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (e) =>{
+    e.preventDefault()
+    submitBtn(e.target.newtaskdescription.value)
+  })
 });
+function submitBtn(task){
+  let ul = document.getElementById("tasks")
+  let li = document.createElement("li")
+  let x = document.createElement("button")
+  x.addEventListener("click",deleteElem)
+   li.innerText = `${task}`
+   x.innerHTML = "X"
+   ul.appendChild(li)
+   li.appendChild(x)
+
+}
+function deleteElem(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
